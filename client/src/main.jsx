0d464d6f9aeb94cb8a79d9bf0422664dad185f71
Zelalem Getahun <zelalemg935@gmail.com>
2026-08-12 17:43:28 +0300

    integrated clerk

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,24 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { store } from './app/store.js'
-import { Provider } from 'react-redux'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
+import { ClerkProvider } from "@clerk/react";
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-    </BrowserRouter>,
-)
+// Fetch the publishable key from Vite's env variables
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk Publishable Key in .env file");
+}
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ClerkProvider>
+  </BrowserRouter>,
+);
